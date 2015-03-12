a23f62399c02c4775a16ad589c963cdc422ca5e4
volnoboy <volnoboy@gmail.com>
2015-03-12 19:02:49 +0200

    applicationContext and setter injection

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,14 @@
 import com.volnoboy.service.CustomerService;
-import com.volnoboy.service.CustomerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by Reuven on 3/8/15.
  */
 public class Application {
 	public static void main(String[] args) {
-		CustomerService customerService = new CustomerServiceImpl();
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		CustomerService customerService = applicationContext.getBean("customerService", CustomerService.class);
 		System.out.println(customerService.findAll().get(0).getFirstName());
 	}
 
--- src/main/java/com/volnoboy/service/CustomerServiceImpl.java
+++ src/main/java/com/volnoboy/service/CustomerServiceImpl.java
@@ -1,17 +1,23 @@
 package com.volnoboy.service;
 
+import java.util.List;
+
 import com.volnoboy.model.Customer;
 import com.volnoboy.repository.CustomerRepository;
-import com.volnoboy.repository.HibernateCustomerRepositoryImpl;
-
-import java.util.LinkedHashMap;
-import java.util.List;
 
 /**
  * Created by Reuven on 3/8/15.
  */
 public class CustomerServiceImpl implements CustomerService {
-	private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+	private CustomerRepository customerRepository;
+
+	public CustomerRepository getCustomerRepository() {
+		return customerRepository;
+	}
+
+	public void setCustomerRepository(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 	@Override
 	public List<Customer> findAll() {
