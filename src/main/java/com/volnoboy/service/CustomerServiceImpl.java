2b263254a164cc9545b88382898ff3ddbb97a7a3
volnoboy <volnoboy@gmail.com>
2015-03-12 19:10:38 +0200

    constructor injection

--- src/main/java/com/volnoboy/service/CustomerServiceImpl.java
+++ src/main/java/com/volnoboy/service/CustomerServiceImpl.java
@@ -11,6 +11,14 @@ import com.volnoboy.repository.CustomerRepository;
 public class CustomerServiceImpl implements CustomerService {
 	private CustomerRepository customerRepository;
 
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
+	public CustomerServiceImpl() {
+
+	}
+
 	public CustomerRepository getCustomerRepository() {
 		return customerRepository;
 	}
